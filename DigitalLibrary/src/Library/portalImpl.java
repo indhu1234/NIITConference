06a47ffd9e367b647f7bb759259f95ff351e5bf2
MR USER <MR USER@DESKTOP-9UC000P>
2019-08-10 17:09:16 +0530

    Commits interface

--- DigitalLibrary/src/Library/portalImpl.java
+++ DigitalLibrary/src/Library/portalImpl.java
@@ -1,6 +1,19 @@
 package Library;
 
-public class portalImpl 
+public class portalImpl implements portal
 {
-   
+     
+	@Override
+	public String searchbyauthor(String author)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String searchbybooks(int bid)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
